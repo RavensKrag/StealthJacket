b46520c3a4de7916a9e4a7a40f243b3ca0e3f27e
Raven <avantflux.raven@gmail.com>
2013-07-30 19:43:14 -0400

    Get jacket and mobster to show up again. Positions not guaranteed exact.

--- Assets/Scripts/HearingAlert.js
+++ Assets/Scripts/HearingAlert.js
@@ -1,17 +1,29 @@
 var parent_object;
 
+var target_object;
+
+var turn_speed : double;
+
 function Start () {
 	parent_object = transform.parent.gameObject;
+	target_object = null;
 }
 
 function Update () {
-	
+	TurnTowardsDisturbance();
 }
 
 function OnTriggerEnter(c : Collider) {
 	// print("FINISH");
+	target_object = c.gameObject;
+}
+
+function OnTriggerExit (c : Collider) {
+	target_object = null;
 }
 
 function TurnTowardsDisturbance() {
-	
+	if(target_object != null) {
+		
+	}
 }
